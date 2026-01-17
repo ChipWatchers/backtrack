4fcec95e7e959be79deac2738b8c2ae1170c7dc4
naa-siuuuu-ff <nasifhasan2005@gmail.com>
2026-01-17 18:44:28 +0800

    fix: use dynamic port

--- telegram/bot.js
+++ telegram/bot.js
@@ -142,7 +142,7 @@ function loadFriends() {
 }
 
 function startHttpServer() {
-  const PORT = 3001;
+  const PORT = process.env.PORT || 3001;
 
   const server = http.createServer(async (req, res) => {
     // Handle CORS
